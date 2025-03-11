a4883c26fc1e626c3efb42a4644989cd909f93a8
24025817 <eduardochenzou@gmail.com>
2025-03-10 22:42:30 -0300

    adicionando o arquivo de exceção

--- src/SecurityVoice/app/src/main/java/com/ubercab/exceptions/LengthRGException.java
+++ src/SecurityVoice/app/src/main/java/com/ubercab/exceptions/LengthRGException.java
@@ -3,6 +3,6 @@ package com.ubercab.exceptions;
 public class LengthRGException extends RuntimeException {
     public LengthRGException(String message) {
         super(message);
-        System.out.println(message);
+        System.out.println(message
     }
 }
